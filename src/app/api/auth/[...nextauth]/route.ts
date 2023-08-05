186d6ff2f6222262df38cc6e2dce4a18c779a020
NaveenPrajapati <solidauto202@gmail.com>
2023-08-05 11:09:41 +0530

    check again session type

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,7 +17,10 @@ const handler = NextAuth({
       async session({ session }) {
         // store the user id from MongoDB to session
         const sessionUser = await User.findOne({ email: session?.user?.email });
-        session?.user?.id = sessionUser._id.toString();
+        const userId = session?.user?.id;
+        if (userId) {
+          session.user.id = sessionUser._id.toString();
+        }
   
         return session;
       },
