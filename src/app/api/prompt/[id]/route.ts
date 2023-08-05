fc35136449ec592ba0b0b4866e4f5f8e70313e65
NaveenPrajapati <solidauto202@gmail.com>
2023-08-05 10:22:44 +0530

    change patch

--- src/app/api/prompt/[id]/route.ts
+++ src/app/api/prompt/[id]/route.ts
@@ -17,7 +17,7 @@ export const GET = async (request: any, { params }: any) => {
     }
 }
 
-export const PATCH = async (request: { json: () => PromiseLike<{ prompt: any; tag: any; }> | { prompt: any; tag: any; }; }, { params }: any) => {
+export const PATCH = async (request ) => {
     const { prompt, tag } = await request.json();
 
     try {
