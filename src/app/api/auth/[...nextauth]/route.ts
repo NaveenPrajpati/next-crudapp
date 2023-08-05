dc8d0217f375a162abcf22f3b56380a6aecf8823
NaveenPrajapati <solidauto202@gmail.com>
2023-08-05 10:55:00 +0530

    change session type error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -16,8 +16,8 @@ const handler = NextAuth({
     callbacks: {
       async session({ session }) {
         // store the user id from MongoDB to session
-        const sessionUser = await User.findOne({ email: session.user.email });
-        session.user.id = sessionUser._id.toString();
+        const sessionUser = await User.findOne({ email: session?.user?.email });
+        session?.user?.id = sessionUser._id.toString();
   
         return session;
       },
@@ -27,19 +27,19 @@ const handler = NextAuth({
         
   
           // check if user already exists
-          const userExists = await User.findOne({ email: profile.email });
+          const userExists = await User.findOne({ email: profile?.email });
   
           // if not, create a new document and save user in MongoDB
           if (!userExists) {
             await User.create({
-              email: profile.email,
-              username: profile.name.replace(" ", "").toLowerCase(),
-              image: profile.picture,
+              email: profile?.email,
+              username: profile?.name.replace(" ", "").toLowerCase(),
+              image: profile?.picture,
             });
           }
   
           return true
-        } catch (error) {
+        } catch (error:any) {
           console.log("Error checking if user exists: ", error.message);
           return false
         }
